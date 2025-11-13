1d7b01f62dc72328e85715b317105fb3ccdf9bad
adityaa1205 <145536614+adityaa1205@users.noreply.github.com>
2025-11-13 12:20:02 +0530

    testing

--- backend/pet_rescue_pro/pet_rescue_pro/settings.py
+++ backend/pet_rescue_pro/pet_rescue_pro/settings.py
@@ -154,8 +154,8 @@ ALLOWED_HOSTS = ['pet-adoption-and-management-system.onrender.com', 'localhost',
 
 # ==== Email Settings ====
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_HOST = "smtp.mailersend.net"
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
